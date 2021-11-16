d65ba4825a5bc072f1931e43f1622bbf2627d346
Andrew Thomas <ajthomas145@gmail.com>
2021-11-16 20:41:52 +0000

    mvp tested and passed

---
+++ src/main/java/Library.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> books;
+    private int capacity;
+
+    public Library(){
+        this.books = new ArrayList<>();
+        this.capacity = 5;
+        }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+           }
+
+    public int countBooks() {
+        return this.books.size();
+    }
+
+    public boolean freeSpaceInLibrary() {
+        if (this.capacity > this.books.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void checkCapacityBeforeAdding(Book book) {
+        if (this.freeSpaceInLibrary() == true) {
+            addBook(book);
+        }
+
+    }
+
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,62 @@
+import jdk.jfr.StackTrace;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void setUp(){
+        library = new Library();
+        book = new Book("How to code", "Zsolt", "Coding");
+    }
+
+    @Test
+    public void addBookToLibrary(){
+        library.addBook(book);
+        assertEquals(1, library.countBooks());
+    }
+
+    @Test
+    public void canBookBeAddedToLibrary(){
+        assertEquals(true, library.freeSpaceInLibrary());
+    }
+
+    @Test
+    public void BookCantBeAddedToLibrary(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        assertEquals(false, library.freeSpaceInLibrary());
+
+    }
+
+    @Test
+    public void capacityInLibraryToAddBook(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.checkCapacityBeforeAdding(book);
+        assertEquals(4, library.countBooks());
+    }
+
+    @Test
+    public void noCapacityInLibraryToAddBook(){
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.addBook(book);
+        library.checkCapacityBeforeAdding(book);
+        assertEquals(5, library.countBooks());
+
+    }
+
+
+}
